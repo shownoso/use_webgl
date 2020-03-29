a74a36f7e6ce2d1e742dd4c3848fbb2b52fba99f
shownoso <457802359@qq.com>
2020-03-29 23:22:32 +0800

    add spotlight and enabled shadow

--- use_three/index.js
+++ use_three/index.js
@@ -15,8 +15,9 @@ camera.lookAt(scene.position);
 let renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setClearColor(new THREE.Color(0x000000));
 renderer.setSize(window.innerWidth, window.innerHeight);
-
-
+// 开启阴影 高消耗
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 // 添加辅助工具
 let axes = new THREE.AxesHelper(200);
 scene.add(axes);
@@ -24,7 +25,7 @@ scene.add(axes);
 
 // 创建一个平面
 let planeGeometry = new THREE.PlaneGeometry(70, 50, 1, 1);
-let planeMaterial = new THREE.MeshBasicMaterial({
+let planeMaterial = new THREE.MeshLambertMaterial({
   color: 0xfff566, 
   side: THREE.DoubleSide
 });
@@ -33,34 +34,45 @@ plane.rotation.x = -0.5 * Math.PI;
 plane.position.x = 15;
 plane.position.y = 0;
 plane.position.z = 0;
+// 平面接受投射过来的阴影
+plane.receiveShadow = true;
 scene.add(plane);
 
 // 创建一个立方体
 
 let cubeGeometry = new THREE.CubeGeometry(5, 5, 5);
-let cubeMaterial = new THREE.MeshBasicMaterial({
+let cubeMaterial = new THREE.MeshLambertMaterial({
   color: 0x1890ff,
-  wireframe: true
+  // wireframe: true
 });
 let cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 cube.position.x = -4;
 cube.position.y = 3;
 cube.position.z = 0;
+// 投射阴影
+cube.castShadow = true;
 scene.add(cube);
 
 // 创建一个球
 let sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
-let sphereMaterial = new THREE.MeshBasicMaterial({
+let sphereMaterial = new THREE.MeshLambertMaterial({
   color: 0xf5222d,
-  wireframe: true
+  // wireframe: true
 });
 let sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 sphere.position.x = 20;
 sphere.position.y = 4;
 sphere.position.z = -2;
+// 投射阴影
+sphere.castShadow = true;
 scene.add(sphere);
 
-
+// 创建点光源
+var spotLight = new THREE.SpotLight(0xffffff);
+spotLight.position.set(-40, 60, -10);
+// 投射阴影
+spotLight.castShadow = true;
+scene.add( spotLight );
 
 document.body.appendChild( renderer.domElement );
 renderer.render(scene, camera);
